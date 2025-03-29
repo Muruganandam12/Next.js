dfc4a5e279c54e5c5c9f49ea33f92ba1b29e789d
Muruganandam12 <murugamuruga96561@gmail.com>
2025-03-29 12:42:07 +0530

    Next Js Intro New

---
+++ my-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-center text-2xl font-bold">Next Application</h1>
+    </main>
+  );
+}
